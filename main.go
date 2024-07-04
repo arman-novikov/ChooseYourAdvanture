71ee9ea2db40b6b7bc07623b64351d44b53c0d45
Novikov, Arman <arman.novikov@intel.com>
2024-07-04 13:36:31 +0200

    main refactored: extracted funcs to cover with unit tests

--- main.go
+++ main.go
@@ -9,6 +9,22 @@ import (
 	"strconv"
 )
 
+func main() {
+	story_json_file_name := parseArgs()
+	story := parseJsonStory(story_json_file_name)
+	arc := getIntro(story)
+
+	for len(arc.Options) > 0 {
+		showArc(arc)
+		showOptions(arc.Options)
+		choice := getUserChoice(arc.Options)
+		chosen_arc := arc.Options[choice].Arc
+		arc = getChosenArc(story, chosen_arc)
+	}
+
+	fmt.Println("The end")
+}
+
 type ArcOption struct {
 	Text string
 	Arc  string
@@ -20,55 +36,67 @@ type Arc struct {
 	Options []ArcOption
 }
 
-func main() {
+func parseArgs() string {
 	story_json_file_name := flag.String("story", "gopher.json", "json file with the story")
 	flag.Parse()
-	story_json, err := os.ReadFile(*story_json_file_name)
+	return *story_json_file_name
+}
+
+func parseJsonStory(story_json_file_name string) map[string]Arc {
+	story_json, err := os.ReadFile(story_json_file_name)
 	if err != nil {
-		log.Fatalf("Failed to open file %s: %e", *story_json_file_name, err)
+		log.Fatalf("Failed to open file %s: %e", story_json_file_name, err)
 	}
 
 	var story map[string]Arc
 	err = json.Unmarshal(story_json, &story)
 	if err != nil {
-		log.Fatalf("Failed to unmarshal file %s: %e", *story_json_file_name, err)
+		log.Fatalf("Failed to unmarshal file %s: %e", story_json_file_name, err)
 	}
+	return story
+}
 
+func getIntro(story map[string]Arc) Arc {
 	arc, found := story["intro"]
 	if !found {
 		log.Fatalf("failed to find intro in story. Make sure your story has 'intro' section")
 	}
-	for {
-		fmt.Println(arc.Title)
-		for _, piece := range arc.Story {
-			fmt.Println(piece)
-		}
+	return arc
+}
 
-		if len(arc.Options) == 0 {
+func showArc(arc Arc) {
+	fmt.Println(arc.Title)
+	for _, piece := range arc.Story {
+		fmt.Println(piece)
+	}
+}
+
+func showOptions(options []ArcOption) {
+	for i, arcOption := range options {
+		fmt.Printf("%d) %s\n", i, arcOption.Text)
+	}
+}
+
+func getUserChoice(options []ArcOption) int {
+	var choice int
+	for {
+		var rawChoice string
+		fmt.Printf("Your choice: ")
+		fmt.Scanln(&rawChoice)
+		choice, err := strconv.Atoi(rawChoice)
+		if err == nil && choice < len(options) {
+			fmt.Printf("You choosed %s\n", options[choice].Arc)
 			break
 		}
-		for i, arcOption := range arc.Options {
-			fmt.Printf("%d) %s\n", i, arcOption.Text)
-		}
-
-		var choice int
-		for {
-			var rawChoice string
-			fmt.Printf("Your choice: ")
-			fmt.Scanln(&rawChoice)
-			choice, err = strconv.Atoi(rawChoice)
-			if err == nil && choice < len(arc.Options) {
-				fmt.Printf("You choosed %s\n", arc.Options[choice].Arc)
-				break
-			}
-			fmt.Printf("invalid input. Try again.")
-		}
-		next_arc := arc.Options[choice].Arc
-		arc, found = story[next_arc]
-		if !found {
-			log.Fatalf("Failed to find arc: %s", next_arc)
-		}
+		fmt.Printf("invalid input. Try again.")
 	}
+	return choice
+}
 
-	fmt.Println("The end")
+func getChosenArc(story map[string]Arc, next_arc string) Arc {
+	arc, found := story[next_arc]
+	if !found {
+		log.Fatalf("Failed to find arc: %s", next_arc)
+	}
+	return arc
 }
